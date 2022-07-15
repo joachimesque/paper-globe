20e0fb0a636136f6b7b99cd233fcb63fde4fae41
Joachim <joachim.robert@protonmail.com>
2022-07-15 09:52:57 +0200

    Add package info 👯

--- setup.py
+++ setup.py
@@ -2,7 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="paperglobe",
+    description="Turns a map into a paper globe template",
+    long_description="A Python utility that will transform an image of the Earth to a template that you can cut and assemble by yourself",
+    url="https://github.com/joachimesque/paper-globe",
     version="0.0.1",
+    author="Joachim Robert",
+    author_email="joachim.robert@protonmail.com",
+    license="GNU AGPL 3",
     py_modules=["paperglobe"],
     packages=find_packages(),
     include_package_data=True,
